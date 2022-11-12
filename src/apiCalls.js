cd6829733a46705d03416f39664ea02b8beba2dc
Michael Martinelli <turtelboy123@gmail.com>
2022-11-11 22:40:59 -0700

    change the path for the fetch call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
 const fetchData = async () => {
-    const response = await fetch("http://ddragon.leagueoflegends.com/cdn/12.18.1/data/en_US/champion.json");
+    const response = await fetch("https://ddragon.leagueoflegends.com/cdn/12.18.1/data/en_US/champion.json");
     const json = await response.json();
     const data = Object.keys(json.data).map((x) => json.data[x]);
     return data;
